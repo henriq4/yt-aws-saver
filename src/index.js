0f1ce1b587481ecb9c8f282d9b9d1f8439df4def
Henrique GC <henriq4NET@gmail.com>
2022-12-20 14:49:50 -0300

    feat: implementing  differents downloads for  audio and video tracks with ffmpeg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,53 @@
-import fs from 'fs'
-import ytdl from 'ytdl-core';
+import cp from "child_process";
+import ytdl from "ytdl-core";
+import ffmpeg from "ffmpeg-static";
+import { question } from "readline-sync";
 
-ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
-  .pipe(fs.createWriteStream('video.flv'));
+const url = question("youtube url: ");
+
+const tracker = {
+  start: new Date(),
+};
+
+// Get audio and video streams
+const audio = ytdl(url, { quality: "highestaudio" });
+const video = ytdl(url, { quality: "highestvideo" });
+
+// Start the ffmpeg child process
+const ffmpegProcess = cp.spawn(ffmpeg, [
+  "-loglevel", "8", "-hide_banner",
+  "-progress", "pipe:3",
+  "-i", "pipe:4",
+  "-i", "pipe:5",
+  "-map", "0:a",
+  "-map", "1:v",
+  "-c:v", "copy",
+  `tmp/${tracker.start.toISOString().split(".")[0]}.mkv`,
+], {
+  windowsHide: true,
+  stdio: [
+    /* stdin, stdout, stderr */
+    "inherit", "inherit", "inherit",
+    /* pipe:3, pipe:4, pipe:5 */
+    "pipe", "pipe", "pipe",
+  ],
+});
+ffmpegProcess.on("close", () => {
+  console.log("done");
+});
+
+// Link streams
+ffmpegProcess.stdio[3].on("data", chunk => {
+  const lines = chunk.toString().trim().split("\n");
+  const args = {};
+
+  for (const l of lines) {
+    const [key, value] = l.split("=");
+    args[key.trim()] = value.trim();
+  }
+
+  tracker.merged = args;
+});
+
+audio.pipe(ffmpegProcess.stdio[4]);
+video.pipe(ffmpegProcess.stdio[5]);
