2d22565024c9d5a69cdde4e2504c9543e36e6af9
Henrique GC <henriq4net@gmail.com>
2023-01-05 18:15:46 -0300

    chore: adding redis to env variables

--- src/config/redis/index.js
+++
@@ -1,5 +0,0 @@
-export const redisConfig = {
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSW
-};
---
+++ src/config/redis/index.ts
@@ -0,0 +1,7 @@
+const redisConfig = {
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSW,
+};
+
+export { redisConfig };
