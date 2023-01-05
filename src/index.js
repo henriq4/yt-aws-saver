fd4a8255d1351681cc4499b90b74749e41060cc8
Henrique GC <henriq4net@gmail.com>
2023-01-05 15:38:45 -0300

    refactor: chande index to run with aws upload from lib-storage and pass through from nodejs native streams

--- src/index.js
+++ src/index.js
@@ -1,91 +1,43 @@
-import {config} from "dotenv";
-config();
-
-import cp from "child_process";
+import { PassThrough } from "stream";
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client } from "@aws-sdk/client-s3";
 import ytdl from "ytdl-core";
-import ffmpeg from "ffmpeg-static";
-import { question } from "readline-sync";
-import { resolve } from "path";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import fs from "fs";
 
-const url = question("youtube url: ");
+const passThrough = new PassThrough();
 
-const tracker = {
-  start: new Date(),
-};
+const url = "https://www.youtube.com/watch?v=1J6Ks4GE9cg";
 
-const filename = `${tracker.start.toISOString().split(".")[0]}.mkv`;
+async function test() {
+  let info = await ytdl.getInfo(url);
 
-const s3Client = new S3Client({
-  region: process.env.AWS_DEFAULT_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
-});
-
-export const run = async () => {
-  const originalPath = resolve("./", "tmp/", filename);
-  const fileContent = await fs.promises.readFile(originalPath);
-
-  const bucketParams = {
-    Bucket: process.env.AWS_BUCKET,
-    Key: filename,
-    Body: fileContent,
-  };
+  const download = ytdl(url, {
+    quality: "highest",
+  }).pipe(passThrough);
 
   try {
-    await s3Client.send(new PutObjectCommand(bucketParams));
+    const parallelUploads3 = new Upload({
+      client: new S3Client({
+        region: process.env.AWS_DEFAULT_REGION,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        },
+      }),
+      params: {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${info.videoDetails.title.split(" ").join("_")}.mkv`,
+        Body: download,
+      },
+    });
 
-    await fs.promises.unlink(originalPath);
-  } catch (err) {
-    console.log("Error", err);
+    parallelUploads3.on("httpUploadProgress", progress => {
+      console.log(progress);
+    });
+
+    await parallelUploads3.done();
+  } catch (e) {
+    console.log(e);
   }
-};
+}
 
-// Get audio and video streams
-const audio = ytdl(url, { quality: "highestaudio" });
-const video = ytdl(url, { quality: "highestvideo" });
-
-// Start the ffmpeg child process
-const ffmpegProcess = cp.spawn(ffmpeg, [
-  "-loglevel", "8", "-hide_banner",
-  "-progress", "pipe:3",
-  "-i", "pipe:4",
-  "-i", "pipe:5",
-  "-map", "0:a",
-  "-map", "1:v",
-  "-c:v", "copy",
-  `tmp/${filename}`,
-], {
-  windowsHide: true,
-  stdio: [
-    /* stdin, stdout, stderr */
-    "inherit", "inherit", "inherit",
-    /* pipe:3, pipe:4, pipe:5 */
-    "pipe", "pipe", "pipe",
-  ],
-});
-
-ffmpegProcess.on("close", async () => {
-  console.log("done\n");
-  await run();
-  console.log("\nsended\n");
-});
-
-// Link streams
-ffmpegProcess.stdio[3].on("data", chunk => {
-  const lines = chunk.toString().trim().split("\n");
-  const args = {};
-
-  for (const l of lines) {
-    const [key, value] = l.split("=");
-    args[key.trim()] = value.trim();
-  }
-
-  tracker.merged = args;
-});
-
-audio.pipe(ffmpegProcess.stdio[4]);
-video.pipe(ffmpegProcess.stdio[5]);
+test();
