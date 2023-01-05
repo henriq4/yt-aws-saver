fddf19319b408fa0d6477db072e6be3b95f75370
Henrique GC <henriq4net@gmail.com>
2023-01-05 18:35:51 -0300

    chore: adding path.resolve to directories

---
+++ src/utils/dir/index.ts
@@ -0,0 +1,11 @@
+import path from "path";
+
+const dir = {
+  rootDir: path.resolve(__dirname, "../../../"),
+  srcDir: path.resolve(__dirname, "../../"),
+  servicesDir: path.resolve(__dirname, "../../services/"),
+  configDir: path.resolve(__dirname, "../../config/"),
+  utilsDir: path.resolve(__dirname, "../../utils/"),
+};
+
+export { dir };
