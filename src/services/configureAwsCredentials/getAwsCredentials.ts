1cb5c12ff82676e8ae774ec67c6dc4279f388f8f
Henrique GC <henriq4net@gmail.com>
2023-01-06 08:51:37 -0300

    chore: show logs for user

--- src/services/configureAwsCredentials/getAwsCredentials.ts
+++ src/services/configureAwsCredentials/getAwsCredentials.ts
@@ -8,12 +8,19 @@ const getAwsCredentials = async () => {
     terminal: false,
   });
 
+  // alert message
+  console.log(
+    "*************************************\nParece que suas credenciais AWS não estão configuradas\n*************************************\n",
+  );
+
   const accessKeyId = await rl.question("Aws access key id: ");
   const secretAccessKey = await rl.question("Aws secret for access key: ");
   const region = await rl.question("Aws IAM account region: ");
   const bucket = await rl.question("Aws s3 bucket: ");
 
   rl.close();
+
+  console.log("Credenciais salvas");
 
   return {
     accessKeyId,
